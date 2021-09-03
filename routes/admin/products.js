d51c146e770945295c5288cd2f3f1bf60a09634b
Paolo Micheletti <paulmic20@gmail.com>
2021-09-03 09:16:48 +0200

    post and get on edit elements. handlederrors

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -4,6 +4,7 @@ const multer = require('multer')
 const { handleErrors, requireAuth } = require('./middleware')
 const productsRepo = require('../../repositories/product')
 const productsNewTemplate = require('../../views/admin/products/new')
+const productsEditTemplate = require('../../views/admin/products/edit')
 const productsIndexTemplate = require('../../views/admin/products/index')
 const { requireTitle, requirePrice } = require('./validator')
 
@@ -32,6 +33,39 @@ router.post(
     await productsRepo.create({ title, price, image })
 
     res.redirect('/admin/products')
+})
+
+router.get('/admin/products/:id/edit', requireAuth, async (req,res) => {
+    const product = await productsRepo.getOne(req.params.id)
+
+    if (!product) {
+       return res.send('Product not found')
+    }
+
+    res.send(productsEditTemplate({ product }))
+})
+
+router.post(
+    '/admin/products/:id/edit',
+    requireAuth, upload.single('imsge'),
+    [requireTitle, requirePrice],
+    handleErrors(productsEditTemplate, async req => {
+        const product = await productsRepo.getOne(req.params.id);
+        return { product}
+    }),
+    async (req,res) => {
+        const changes = req.body;
+
+        if (req.file) {
+            changes.image = req.file.buffer.toString('base64')
+        }
+        try {
+            await productsRepo.update(req.params.id, changes)
+        }
+        catch (err) {
+            return res.send('Cuould not find item')
+        }
+
 })
 
 module.exports = router
